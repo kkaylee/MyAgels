2f682457e365905cd0d31592461f71b126788249
kkaylee <kaylee.kappes@patelhs.org>
2020-09-23 14:02:17 +0000

    I put in int plus myAge = 14 theni printed out my statement which is saying "i am' plus myAge "years old" then i ran it.

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int myAge = 14;
+    System.out.println("I am " + myAge + " years old");
   }
 }
